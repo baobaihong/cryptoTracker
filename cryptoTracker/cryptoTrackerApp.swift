c10525ae0903b5cf7842bcc4c6d8c6a2f6233b56
Jason Bao <baihongbao@outlook.com>
2024-02-17 11:24:58 +0800

    Initial Commit

---
+++ cryptoTracker/cryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoTrackerApp.swift
+//  cryptoTracker
+//
+//  Created by Jason on 2024/2/17.
+//
+
+import SwiftUI
+
+@main
+struct cryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
