4454e50f7949874213cef9fcacac9646ab0f5c5f
Jason Bao <baihongbao@outlook.com>
2024-02-19 11:32:34 +0800

    Create reusable networking publisher and error handler

--- cryptoTracker/Services/CoinDataService.swift
+++ cryptoTracker/Services/CoinDataService.swift
@@ -19,27 +19,15 @@ class CoinDataService {
     private func getCoins() {
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h&locale=en") else { return }
         
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { output -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
+        // create a combine publisher that asynchronously fetching data from internet.
+        // when the task complete, it publishes either a tuple that contains the fetched data and a URLResponse, or an error if the task fails
+        coinSubscription = NetworkingManager.download(url: url)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .sink { completion in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { [weak self] returnedCoins in
-                self?.allCoins = returnedCoins
-                self?.coinSubscription?.cancel()
-            }
+            .sink(
+                receiveCompletion: NetworkingManager.handleCompletion,
+                receiveValue: { [weak self] returnedCoins in
+                    self?.allCoins = returnedCoins
+                    self?.coinSubscription?.cancel()
+                })
     }
 }
