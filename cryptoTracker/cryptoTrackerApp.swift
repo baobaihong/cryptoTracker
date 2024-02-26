9cf806c34917aa866cafda229b6cfde5987dd335
Jason Bao <baihongbao@outlook.com>
2024-02-26 15:31:45 +0800

    adding launch screen

--- cryptoTracker/cryptoTrackerApp.swift
+++ cryptoTracker/cryptoTrackerApp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 @main
 struct cryptoTrackerApp: App {
     @State private var vm = HomeViewModel()
+    @State private var showLaunchView: Bool = true
     
     // change the navigation title's color to match the color theme
     init() {
@@ -19,11 +20,21 @@ struct cryptoTrackerApp: App {
     
     var body: some Scene {
         WindowGroup {
-            NavigationStack {
-                HomeView()
-                    .toolbar(.hidden)
+            ZStack {
+                NavigationStack {
+                    HomeView()
+                        .toolbar(.hidden)
+                }
+                .environment(vm)
+                ZStack {
+                    if showLaunchView {
+                        LaunchView(showLaunchView: $showLaunchView)
+                            .transition(.opacity.animation(.default))
+                    }
+                }
+                .zIndex(2.0)
             }
-            .environment(vm)
+            
         }
     }
 }
